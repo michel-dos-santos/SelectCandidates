373b783dafe67a603bf96c15e10f7f550c181df7
Michel dos Santos <michel@debian>
2019-08-29 22:31:58 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/vagas/SelectCandidates/SelectCandidatesApplication.java
@@ -0,0 +1,13 @@
+package br.com.vagas.SelectCandidates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectCandidatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectCandidatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vagas/SelectCandidates/SelectCandidatesApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vagas.SelectCandidates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelectCandidatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
